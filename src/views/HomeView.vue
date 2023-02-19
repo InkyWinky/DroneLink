935eb760a5953f44d1033c50f2dd11fd8b358bf0
InkyWinky <46879999+InkyWinky@users.noreply.github.com>
2023-02-19 16:32:21 +1100

    missed files from previous commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <WaypointsList />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+import WaypointsList from "@/components/waypoints/WaypointsList.vue";
+export default {
+  name: "HomeView",
+  components: {
+    WaypointsList,
+  },
+};
+</script>
+<style>
+body {
+  background-color: #eeeeee;
+  height: 100%;
+}
+div {
+  height: 100%;
+}
+</style>
