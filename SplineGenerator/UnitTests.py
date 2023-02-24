a98da2f989377e01f0669562092a6f6570cc7342
Jasraj4354 <jbha0006@student.monash.edu>
2023-02-24 23:01:29 +1100

    test cases added. all passed

--- SplineGenerator/UnitTests.py
+++ SplineGenerator/UnitTests.py
@@ -16,7 +16,10 @@ class UnitTests(unittest.TestCase):
         # {'waypoints': [], 'radius': 0.0, 'boundary': [], 'boundary_res': 0, 'tolerance': 0.0, 'curve_resolution': 0.0}
         # {'waypoints': [[1, 1], [2, 3], [5, 7], [7, 2], [2, -4]], 'radius': (0.9, 1.9), 'boundary': None, 'boundary_resolution': None, 'tolerance': 0.0, 'curve_resolution': 3.0}
         test_cases = [{'waypoints': [[1, 1], [2, 3], [5, 7], [7, 2], [2, -4]], 'radius': (0.9, 1.9), 'boundary': None, 'boundary_resolution': None, 'tolerance': 0.0, 'curve_resolution': 3.0},
-                      {'waypoints': [[1, 1], [3, 3], [5, 5], [7, 7], [5, -4]], 'radius': (0.9, 1.9), 'boundary': None, 'boundary_resolution': None, 'tolerance': 0.0, 'curve_resolution': 1.7}
+                      {'waypoints': [[1, 1], [3, 3], [5, 5], [7, 7], [5, -4]], 'radius': (0.9, 1.9), 'boundary': None, 'boundary_resolution': None, 'tolerance': 0.0, 'curve_resolution': 1.7},
+                      {'waypoints': [[1, 2], [3, 6], [5, 9], [7, 8], [5, -2]], 'radius': (0.9, 1.9), 'boundary': None, 'boundary_resolution': None, 'tolerance': 0.0, 'curve_resolution': 1.7},
+                      {'waypoints': [[1, 3], [3, 2], [3, 8], [5, 4], [2, -4]], 'radius': (0.9, 1.9), 'boundary': None, 'boundary_resolution': None, 'tolerance': 0.0, 'curve_resolution': 1.7},
+                      {'waypoints': [[1, 1], [1, 5], [3, 2], [5, -4], [3, 6]], 'radius': (0.9, 1.9), 'boundary': None, 'boundary_resolution': None, 'tolerance': 0.0, 'curve_resolution': 2.0}
                       ]
         for test_case in test_cases:
             waypoint_classed = [Waypoint(point[0], point[1]) for point in test_case['waypoints']]
