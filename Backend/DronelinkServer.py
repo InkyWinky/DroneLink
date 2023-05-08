4a5489fb11f83e96803691e8adf26c3c5e393c8e
haotongwang <nicholas.wang.h@gmail.com>
2023-05-08 13:50:25 +1000

    Add do_OPTIONS to DronelinkServer
    fetch uses an initial OPTIONS request to fetch the headers

--- Backend/DronelinkServer.py
+++ Backend/DronelinkServer.py
@@ -2,45 +2,50 @@ from __future__ import print_function, division
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import json
 import SplineGenerator.SplineGenerator as spline
-import ast
 import time
+
 
 class ServerHandler(BaseHTTPRequestHandler):
     def do_HEAD(self):
         self.send_response(200)
         self.send_header("Content-Type", "application/json")
-        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header("Access-Control-Allow-Origin", "*")
         self.send_header(
-            'Access-Control-Allow-Headers',
-            'Origin, X-Requested-With, Content-Type, Accept'
+            "Access-Control-Allow-Headers",
+            "Origin, X-Requested-With, Content-Type, Accept",
         )
         self.end_headers()
+
+    def do_OPTIONS(self):
+        self.do_HEAD()
 
     def do_POST(self):
         # Send initial headers
         self.do_HEAD()
 
         # Get the message from API client
-        content_length = int(self.headers.getheader('content-length', 0))
+        content_length = int(self.headers.getheader("content-length", 0))
         post_message = self.rfile.read(content_length)
 
         # Extract list of waypoints
-        parsed_content = ast.literal_eval(post_message)
+        parsed_content = json.loads(post_message)
         waypoint_list = []
         for waypoint in parsed_content:
-            new_waypoint = [waypoint['long'], waypoint['lat']]
+            new_waypoint = [waypoint["long"], waypoint["lat"]]
             waypoint_list.append(new_waypoint)
 
         # Get list of Waypoint classes
         waypoint_class_list = spline.generate_waypoints_from_list(waypoint_list)
 
         # Create instance of SplineGenerator class
-        spliner = spline.SplineGenerator(waypoints=waypoint_class_list,
-                                         radius_range=(38.1, 39),  # Metres
-                                         boundary_points=None,
-                                         boundary_resolution=None,
-                                         tolerance=0.0,
-                                         curve_resolution=3)
+        spliner = spline.SplineGenerator(
+            waypoints=waypoint_class_list,
+            radius_range=(38.1, 39),  # Metres
+            boundary_points=None,
+            boundary_resolution=None,
+            tolerance=0.0,
+            curve_resolution=3,
+        )
 
         # Get the data in a JSON readable format and send it back to whoever asked for it
         waypoint_output_list = spliner.get_waypoint_in_dictionary()
