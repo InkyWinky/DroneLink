e4c3f41bef291cfe8b2bcff44493355ec09148b3
KevinLu9 <kevin.lu902@gmail.com>
2023-08-06 17:02:04 +1000

    improved UI for connect ip modal and toggle arm button

--- src/App.vue
+++ src/App.vue
@@ -6,33 +6,46 @@
       <i class="fa-sharp fa-solid fa-gears icon-btn-effect" id="settings-icon">
       </i>
     </button>
-    <a class="uk-button uk-button-default" href="#modal-center" uk-toggle
-      >Connect to Mission Planner</a
-    >
+    <a class="uk-button uk-button-default" href="#modal-center" uk-toggle>{{
+      connected_ip.valueOf()
+        ? "IP: " + connected_ip.valueOf()
+        : "Connect to Mission Planner"
+    }}</a>
 
     <div id="modal-center" class="uk-flex-top" uk-modal>
       <div
-        class="uk-modal-dialog uk-modal-body uk-margin-auto-vertical uk-height-max-large"
+        class="uk-modal-dialog uk-modal-body uk-height-max-medium rounded-lg"
       >
         <button class="uk-modal-close-default" type="button" uk-close></button>
-
-        <form @submit="onSubmit">
-          <label>Input IP address of device with Mission Planner:</label>
-          <input v-model="ip.value" :ref="ip.ref" />
-          <p v-if="ip.error">{{ ip.error.message }}</p>
-          <button type="submit">submit</button>
+        <form @submit="onSubmit" class="h-full flex flex-col justify-between">
+          <span class="p-4 w-full">
+            <label class="font-bold text-lg">Connect to Mission Planner</label>
+            <input
+              v-model="ip.value"
+              :class="{ 'uk-form-danger': ip.error }"
+              :ref="ip.ref"
+              placeholder="Input Ip address of device with Mission Planner"
+              class="uk-input"
+            />
+            <p v-if="ip.error" class="text-red-500">
+              {{ ip.error.message }}
+            </p>
+          </span>
+          <span class="flex flex-row-reverse justify-self-end m-0 p-0">
+            <button
+              class="uk-button uk-button-primary bg-blue-600 hover:bg-blue-700 ml-2 mt-2 border-gray-600"
+              type="submit"
+            >
+              Submit
+            </button>
+            <button
+              class="uk-button uk-button-default uk-modal-close bg-gray-400 hover:bg-gray-500 border-gray-600 text-white mx-2 mt-2"
+              type="button"
+            >
+              Cancel
+            </button>
+          </span>
         </form>
-        <p class="uk-text-right">
-          <button
-            class="uk-button uk-button-default uk-modal-close"
-            type="button"
-          >
-            Cancel
-          </button>
-          <button class="uk-button uk-button-primary" type="button">
-            Save
-          </button>
-        </p>
       </div>
     </div>
     <div id="drone-connection">
@@ -42,6 +55,34 @@
   </nav>
   <router-view />
 </template>
+
+<script>
+import { useForm } from "vue-hooks-form";
+import api from "./api";
+import { ref } from "vue";
+export default {
+  setup() {
+    const connected_ip = ref("");
+    const { useField, handleSubmit } = useForm({
+      defaultValues: {},
+    });
+    const ip = useField("ip", {
+      rule: { required: true, min: 7, max: 15 },
+    });
+    function connect(ip) {
+      console.log("ip is: " + ip);
+      connected_ip.value = ip;
+      api.executeCommand("CONNECTIP", { ip: ip });
+    }
+    const onSubmit = (data) => connect(data.ip);
+    return {
+      ip,
+      connected_ip,
+      onSubmit: handleSubmit(onSubmit),
+    };
+  },
+};
+</script>
 
 <style>
 @tailwind base;
@@ -119,26 +160,3 @@ nav a.router-link-exact-active {
   box-shadow: 0 0 5px 2px greenyellow;
 }
 </style>
-<script>
-import { useForm } from "vue-hooks-form";
-import api from "./api";
-export default {
-  setup() {
-    const { useField, handleSubmit } = useForm({
-      defaultValues: {},
-    });
-    const ip = useField("ip", {
-      rule: { required: true, min: 7, max: 15 },
-    });
-    function connect(ip) {
-      console.log("ip is: " + ip);
-      api.executeCommand("CONNECTIP", { ip: ip });
-    }
-    const onSubmit = (data) => connect(data.ip);
-    return {
-      ip,
-      onSubmit: handleSubmit(onSubmit),
-    };
-  },
-};
-</script>
