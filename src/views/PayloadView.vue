5ef66c55d22377420ba0ef9d14e77fd21b617404
elib0003 <elib0003@student.monash.edu>
2023-09-04 16:25:10 +1000

    Implemented beginnings of payload view

---
+++ src/views/PayloadView.vue
@@ -0,0 +1,62 @@
+<!-- 
+  [ ] failsafes:
+      [ ] failsafe 1
+      [ ] failsafe 2
+  [ ] payload info:
+      [ ] ground height
+      [ ] velocity
+  [ ] aircraft info:
+      [ ] aircraft height from takeoff
+      [ ] aircraft ground height
+  [ ] status bar
+  [ ] go button
+ -->
+<template>
+  <div id="payload-info">
+    <!-- info about payload -->
+  </div>
+  <div id="aircraft-info">
+    <!-- info about aircraft -->
+  </div>
+  <div id="status-bar">
+    <span id="payload-status">Status: {{ status }}</span>
+  </div>
+  <div id="failsafe-one">
+    <button @click="failsafeOne()">Failsafe 1</button>
+  </div>
+  <div id="failsafe-two">
+    <button @click="failsafeTwo()">Failsafe 2</button>
+  </div>
+  <button @click="go()">Go</button>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const status = ref("");
+
+status.value = 3;
+
+function go() {
+  if (confirm("Confirm deploy payload?")) {
+    // send message to payload system to execute payload delivery
+  }
+}
+
+function failsafeOne() {
+  if (confirm("Confirm failsafe 1?")) {
+    // send message to payload system
+  }
+}
+
+function failsafeTwo() {
+  if (confirm("Confirm failsafe 2?")) {
+    // send message to payload system
+  }
+}
+</script>
+<style>
+#status-bar {
+  color: blue;
+}
+</style>
