1da7b3ad5bbc5e057d1d715691037946606b57e2
KevinLu9 <kevin.lu902@gmail.com>
2023-10-08 13:46:02 +1100

    actually fixed null state on airspeed

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,7 +4,7 @@
   <div id="toggleBtn"><ToggleBtn /></div>
   <div id="speedIndicator">
     <SpeedIndicator
-      :airspeed="store?.live_data?.airspeed.toFixed(2) || 0"
+      :airspeed="store?.live_data?.airspeed?.toFixed(2) || 0"
       :max="150"
       :min="0"
     />
