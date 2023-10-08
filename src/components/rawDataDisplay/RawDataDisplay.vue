7d89a25edc65805b2d030d0453e4c2e3f4e79c79
KevinLu9 <kevin.lu902@gmail.com>
2023-10-08 13:35:21 +1100

    added lat, lng, DistToHome, distTraveled to frontend from MP and added RAW DATA tab for debugging

---
+++ src/components/rawDataDisplay/RawDataDisplay.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="mb-4 h-[90%] rounded-md">
+    <ul
+      class="divide-y-[1px] divide-gray-500 h-full bg-gray-200 rounded-md overflow-y-scroll"
+    >
+      <li
+        v-for="key in Object.keys(store?.live_data)"
+        :key="key"
+        class="flex gap-3 my-2 px-2"
+      >
+        <p class="italic text-xs w-1/3">{{ key }}</p>
+        <p class="text-end text-sm w-2/3">
+          {{
+            typeof store?.live_data[key] == "number"
+              ? store?.live_data[key].toFixed(4)
+              : store?.live_data[key]
+          }}
+        </p>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { store } from "@/store";
+</script>
