c74583e2afca98942df4bafa2b8887d92302776b
KevinLu9 <kevin.lu902@gmail.com>
2023-11-09 23:37:44 +1100

    modified connection status modal to be more informative

--- src/components/ConnectionStatus.vue
+++ src/components/ConnectionStatus.vue
@@ -1,14 +1,5 @@
 <template>
   <div class="flex items-center justify-start ml-20">
-    <div
-      :id="
-        isWebSocketConnected &&
-        store?.live_data?.ip &&
-        store?.live_data?.ip != 'CONNECT TO MISSION PLANNER'
-          ? 'connection-status-on'
-          : 'connection-status-off'
-      "
-    />
     <p
       v-if="!isLoading"
       id="command-btn"
@@ -18,7 +9,11 @@
     >
       {{
         store.live_data?.ip && isWebSocketConnected
-          ? store.live_data?.ip
+          ? `${
+              store?.live_data?.ip != "CONNECT TO MISSION PLANNER"
+                ? "Mission Planner IP: "
+                : ""
+            } ${store.live_data?.ip}`
           : "No Connected MM WebSocket"
       }}
     </p>
@@ -31,54 +26,133 @@
     >
       LOADING...
     </p>
+    <div
+      class="flex-none"
+      :id="
+        isWebSocketConnected &&
+        store?.live_data?.ip &&
+        store?.live_data?.ip != 'CONNECT TO MISSION PLANNER' &&
+        store.live_data?.drone_connected
+          ? 'connection-status-on'
+          : 'connection-status-off'
+      "
+    />
   </div>
 
   <div id="modal-center" class="uk-flex-top" uk-modal>
-    <div class="uk-modal-dialog uk-modal-body uk-height-max-medium rounded-lg">
+    <div class="uk-modal-dialog uk-modal-body rounded-lg h-fit">
       <button class="uk-modal-close-default" type="button" uk-close></button>
-      <form @submit="onSubmit" class="h-full flex flex-col justify-between">
-        <span class="p-4 w-full">
-          <label class="font-bold text-lg">Connect to Mission Planner</label>
-          <input
-            v-if="!isSuccess.valueOf()"
-            v-model="ip.value"
-            :class="{ 'uk-form-danger': ip.error }"
-            :ref="ip.ref"
-            placeholder="Input Ip address of device with Mission Planner"
-            class="uk-input"
-          />
+      <div class="h-fit overflow-y-auto">
+        <p class="font-bold text-center text-lg">Connections</p>
+        <span class="flex w-full items-center">
           <div
-            v-if="isLoading.valueOf()"
-            uk-spinner
-            class="w-full flex flex-col justify-center items-center"
-          ></div>
-          <p v-if="ip.error" class="text-red-500">
-            {{ ip.error.message }}
-          </p>
-          <p v-if="connectionError.valueOf()" class="text-red-500">
-            Connection Failed, Please try again...
-          </p>
-          <p v-if="isSuccess.valueOf()" class="text-green-500">
-            Connection to {{ store?.live_data.ip }} was Successful!
-          </p>
+            class="flex-none"
+            :id="
+              isWebSocketConnected
+                ? 'connection-status-on'
+                : 'connection-status-off'
+            "
+          />
+          <p>Mission Management WebSocket Server</p>
         </span>
-        <span class="flex flex-row-reverse justify-self-end m-0 p-0">
-          <button
-            v-if="!isSuccess.valueOf()"
-            class="uk-button uk-button-primary bg-blue-600 hover:bg-blue-700 ml-2 mt-2 border-gray-600"
-            type="submit"
-          >
-            Submit
-          </button>
-          <button
-            id="cancel_button"
-            class="uk-button uk-button-default uk-modal-close bg-gray-400 hover:bg-gray-500 border-gray-600 text-white mx-2 mt-2"
-            type="button"
-          >
-            Cancel
-          </button>
+        <span class="flex w-full items-center">
+          <div
+            class="flex-none"
+            :id="
+              store?.live_data?.ip &&
+              store?.live_data?.ip != 'CONNECT TO MISSION PLANNER' &&
+              !isLoading
+                ? 'connection-status-on'
+                : 'connection-status-off'
+            "
+          />
+          <div class="flex">
+            <p>Mission Planner Connection</p>
+            <p class="text-green-500 pl-2">
+              {{
+                store?.live_data?.ip &&
+                store?.live_data?.ip != "CONNECT TO MISSION PLANNER" &&
+                !isLoading
+                  ? store.live_data?.ip
+                  : ""
+              }}
+            </p>
+          </div>
         </span>
-      </form>
+        <span class="flex w-full items-center">
+          <div
+            class="flex-none"
+            :id="
+              store.live_data?.drone_connected
+                ? 'connection-status-on'
+                : 'connection-status-off'
+            "
+          />
+          <p>MAVLINK Connection</p>
+        </span>
+        <form @submit="onSubmit" class="flex flex-col justify-between">
+          <span
+            v-show="
+              isWebSocketConnected.valueOf() &&
+              !(
+                store?.live_data?.ip &&
+                store?.live_data?.ip != 'CONNECT TO MISSION PLANNER' &&
+                !isLoading
+              )
+            "
+            class="p-4 w-full"
+          >
+            <label class="font-bold text-lg">Connect to Mission Planner</label>
+            <input
+              v-if="
+                !(
+                  store?.live_data?.ip &&
+                  store?.live_data?.ip != 'CONNECT TO MISSION PLANNER' &&
+                  !isLoading
+                )
+              "
+              v-model="ip.value"
+              :class="{ 'uk-form-danger': ip.error }"
+              :ref="ip.ref"
+              placeholder="Input Ip address of device with Mission Planner"
+              class="uk-input"
+            />
+            <div
+              v-if="isLoading.valueOf()"
+              uk-spinner
+              class="w-full flex flex-col justify-center items-center"
+            ></div>
+            <p v-if="ip.error" class="text-red-500">
+              {{ ip.error.message }}
+            </p>
+            <p v-if="connectionError.valueOf()" class="text-red-500">
+              Connection Failed, Please try again...
+            </p>
+          </span>
+          <span class="flex flex-row-reverse justify-self-end m-0 p-0">
+            <button
+              v-if="
+                !(
+                  store?.live_data?.ip &&
+                  store?.live_data?.ip != 'CONNECT TO MISSION PLANNER' &&
+                  !isLoading
+                )
+              "
+              class="uk-button uk-button-primary bg-blue-600 hover:bg-blue-700 ml-2 mt-2 border-gray-600"
+              type="submit"
+            >
+              Submit
+            </button>
+            <button
+              id="cancel_button"
+              class="uk-button uk-button-default uk-modal-close bg-gray-400 hover:bg-gray-500 border-gray-600 text-white mx-2 mt-2"
+              type="button"
+            >
+              Cancel
+            </button>
+          </span>
+        </form>
+      </div>
     </div>
   </div>
 </template>
@@ -86,7 +160,7 @@
 <script>
 import { useForm } from "vue-hooks-form";
 import api from "../api";
-import { ref } from "vue";
+import { ref, watch } from "vue";
 import { store } from "../store";
 // import toggleSettingsMenu from "./store";
 
@@ -100,14 +174,59 @@ export default {
     // Connection States
     const isWebSocketConnected = ref(false);
 
+    watch(isWebSocketConnected, async (value) => {
+      // Reset live data if no connection
+      if (!value) {
+        store.live_data = {};
+        isSuccess.value = false;
+      }
+    });
     const { useField, handleSubmit } = useForm({
       defaultValues: {},
     });
     const ip = useField("ip", {
       rule: { required: true, min: 7, max: 15 },
     });
+    let ws_connection = null;
+    const connectWebSocket = () => {
+      if (!ws_connection) {
+        console.log("[INFO] Starting connection to WebSocket Server");
+      }
+      ws_connection = new WebSocket(
+        `ws:${window.location.host.split(":")[0]}:8081`
+      );
+      ws_connection.onmessage = function (event) {
+        event;
+        store.updateLiveData(JSON.parse(event.data));
+        // console.log("live_data_feed: ", JSON.parse(event.data));
+      };
+      ws_connection.onopen = function () {
+        // console.log(event);
+        console.log("[INFO] Successfully connected to the WebSocket server");
+        isWebSocketConnected.value = true;
+      };
+      ws_connection.onclose = function () {
+        console.log(
+          "[INFO] Disconnected from the WebSocket server, retrying connection in 1 second..."
+        );
+        isWebSocketConnected.value = false;
+        setTimeout(() => {
+          connectWebSocket();
+        }, 1000);
+      };
+      ws_connection.onerror = function (event) {
+        console.log(
+          "[ERROR] Connection error occured on the WebSocket server, closing connection...",
+          event
+        );
+        isWebSocketConnected.value = false;
+        ws_connection.close();
+      };
+    };
 
     function connect(ip) {
+      // Attempts to connect to a device that is running Mission Planner with the Communication Script.
+      // Assumes that the client is already connected to the backend WebSocket Server.
       console.log("ip is: " + ip);
       connected_ip.value = ip;
       connectionError.value = false;
@@ -128,34 +247,10 @@ export default {
       console.log({ res });
     }
 
-    console.log("[INFO] Starting connection to WebSocket Server");
-    // console.log(`ws:${window.location.host.split(":")[0]}:8081`);
-    const connection = new WebSocket(
-      `ws:${window.location.host.split(":")[0]}:8081`
-    );
-    connection.onmessage = function (event) {
-      event;
-      store.updateLiveData(JSON.parse(event.data));
-      // console.log("live_data_feed: ", JSON.parse(event.data));
-    };
-    connection.onopen = function () {
-      // console.log(event);
-      console.log("[INFO] Successfully connected to the WebSocket server");
-      isWebSocketConnected.value = true;
-    };
-    connection.onclose = function () {
-      console.log("[INFO] Disconnected from the WebSocket server");
-      isWebSocketConnected.value = false;
-    };
-    connection.onerror = function (event) {
-      console.log(
-        "[ERROR] Connection error occured on the WebSocket server",
-        event
-      );
-      isWebSocketConnected.value = false;
-    };
-
     const onSubmit = (data) => connect(data.ip);
+
+    connectWebSocket();
+
     return {
       ip,
       connected_ip,
@@ -183,16 +278,16 @@ export default {
 }
 #connection-status-on {
   background-color: greenyellow;
-  height: 10px;
-  width: 10px;
+  height: 8px;
+  width: 8px;
   border-radius: 5px;
   float: right;
   box-shadow: 0 0 5px 2px greenyellow;
 }
 #connection-status-off {
   background-color: red;
-  height: 10px;
-  width: 10px;
+  height: 8px;
+  width: 8px;
   border-radius: 5px;
   float: right;
   box-shadow: 0 0 5px 2px red;
