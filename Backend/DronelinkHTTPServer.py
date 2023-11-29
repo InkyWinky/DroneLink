2e27fc735dc1dd913d319f6c863d113a19316c84
Nicholas Dellaportas <njdellaportas@hotmail.com>
2023-11-29 21:04:05 +1100

    Added fly to target and drop payload path generation.
    Also added new commands for path generation types and functions to handle those commands

--- Backend/DronelinkHTTPServer.py
+++ Backend/DronelinkHTTPServer.py
@@ -3,8 +3,10 @@ import json
 import time
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 from CommunicationScript.MissionPlannerSocket import Commands
+from SplineGenerator.SearchPathGenerator import Coord, Polygon
+import SplineGenerator.PointToPointPathGenerator as ptpPG
+import SplineGenerator.PathGenerator as path_generator
 import SplineGenerator.SearchPathGenerator as spliner
-import SplineGenerator.PathGenerator as directSpliner
 from mav_enums import *
 
 class HTTPServerThread(threading.Thread):
@@ -31,6 +33,8 @@ class HTTPServerThread(threading.Thread):
 
 
 class ServerHandler(BaseHTTPRequestHandler):
+    # Path generator class instance only instantiated once
+    path_gen = path_generator.PathGenerator()
 
     def do_HEAD(self):
         self.send_response(200)
@@ -65,6 +69,131 @@ class ServerHandler(BaseHTTPRequestHandler):
 
         # Run command
         command = parsed_content["command"]
+
+        if command == Commands.PLANE_PARAMETER_UPDATE:
+            # Go through each parameter and update it if it is not None
+            take_off_point_key = "take_off_point"                           # dict with "lat" and "long"
+            do_plot_key = "do_plot"                                         # bool
+            minimum_turn_radius_key = "minimum_turn_radius"                 # float in metres
+            curve_resolution_key = "curve_resolution"                       # float in waypoints per metre
+            altitude_key = "altitude"                                       # float in metres
+            search_area_key = "search_area"                                 # list of dicts with "lat "and "long"
+            sensor_size_key = "sensor_size"                                 # dict of "width" and "height" each a float in mm
+            focal_length_key = "focal_length"                               # float in mm
+            paint_overlap_key = "paint_overlap"                             # float
+            paint_radius_key = "paint_radius"                               # float in metres
+            layer_distance_key = "layer_distance"                           # float in metres
+            orientation_key = "orientation"                                 # float in radians
+            waypoints_key = "waypoints"                                     # list of dicts with "lat" and "long"
+            boundary_points_key = "boundary_points"                         # list of dicts with "lat" and "long"
+            boundary_resolution_key = "boundary_resolution"                 # int
+            boundary_tolerance_key = "boundary_tolerance"                   # float in metres
+            plane_location_key = "plane_location"                           # dict with "lat" and "long"
+            plane_bearing_key = "plane_bearing"                             # float in degrees (true bearing)
+            target_location_key = "target_location"                         # dict with "lat" and "long"
+            target_circle_radius_key = "target_circle_radius"               # float in metres
+            minimum_distance_to_start_key = "minimum_distance_to_start"     # float in metres
+            times_to_circle_key = "times_to_circle"                         # float
+
+            # Common data
+            if take_off_point_key in parsed_content:
+                self.path_gen.take_off_point = Coord(lat=parsed_content[take_off_point_key]["lat"], lon=parsed_content[take_off_point_key]["long"])
+            if do_plot_key in parsed_content:
+                self.path_gen.do_plot = parsed_content[do_plot_key]
+
+            # Common parameters
+            if minimum_turn_radius_key in parsed_content:
+                self.path_gen.minimum_turn_radius = parsed_content[minimum_turn_radius_key]  # Metres
+            if curve_resolution_key in parsed_content:
+                self.path_gen.curve_resolution = parsed_content[curve_resolution_key]  # Waypoints per metre on a curve
+            if altitude_key in parsed_content:
+                self.path_gen.alt = parsed_content[altitude_key]  # Altitude to print plots at
+
+            # Search area specific data
+            if search_area_key in parsed_content:
+                coord_list = []
+                for point in parsed_content[search_area_key]:
+                    coord_list.append(Coord(lat=point["lat"], lon=point["long"]))
+                self.path_gen.search_area = Polygon(coord_list)
+
+            # Search area specific parameters
+            if sensor_size_key in parsed_content:
+                width = parsed_content[sensor_size_key]["width"]
+                height = parsed_content[sensor_size_key]["height"]
+                self.path_gen.sensor_size = (width, height)
+            if focal_length_key in parsed_content:
+                self.path_gen.focal_length = parsed_content[focal_length_key]
+            if paint_overlap_key in parsed_content:
+                self.path_gen.paint_overlap = parsed_content[paint_overlap_key]
+            if paint_radius_key in parsed_content:
+                self.path_gen.paint_radius = parsed_content[paint_radius_key]
+            if layer_distance_key in parsed_content:
+                self.path_gen.layer_distance = parsed_content[layer_distance_key]
+            if orientation_key in parsed_content:
+                self.path_gen.orientation = parsed_content[orientation_key]
+
+            # Point-to-point specific data
+            if waypoints_key in parsed_content:
+                waypoints = []
+                for point in parsed_content[waypoints_key]:
+                    waypoints.append(ptpPG.Waypoint(x=point["lat"], y=point["long"]))
+                self.path_gen.waypoints = waypoints
+            if boundary_points_key in parsed_content:
+                waypoints = []
+                for point in parsed_content[boundary_points_key]:
+                    waypoints.append(Coord(lat=point["lat"], lon=point["long"]))
+                self.path_gen.boundary_points = waypoints
+
+            # Point-to-point specific parameters
+            if boundary_resolution_key in parsed_content:
+                self.path_gen.boundary_resolution = parsed_content[boundary_resolution_key]
+            if boundary_tolerance_key in parsed_content:
+                self.path_gen.boundary_tolerance = parsed_content[boundary_tolerance_key]
+
+            # Target specific parameters
+            if plane_location_key in parsed_content:
+                self.path_gen.plane_location = Coord(lat=parsed_content[plane_location_key]["lat"], lon=parsed_content[plane_location_key]["long"])
+            if plane_bearing_key in parsed_content:
+                self.path_gen.plane_bearing = parsed_content[plane_bearing_key]
+            if target_location_key in parsed_content:
+                self.path_gen.target_location = Coord(lat=parsed_content[target_location_key]["lat"], lon=parsed_content[target_location_key]["long"])
+            if target_circle_radius_key in parsed_content:
+                self.path_gen.target_circle_radius = parsed_content[target_circle_radius_key]
+            if minimum_distance_to_start_key in parsed_content:
+                self.path_gen.minimum_distance_to_start = parsed_content[minimum_distance_to_start_key]
+            if times_to_circle_key in parsed_content:
+                self.path_gen.times_to_circle = parsed_content[times_to_circle_key]
+
+        """Handle the different path generation types"""
+        if command == Commands.PATH_GENERATION_SEARCH_AREA:
+            self.path_gen.path_generation_type = path_generator.PathGenerationType.SEARCH_AREA
+            path_points = self.path_gen.generate_path()
+            if path_points is not None:
+                mp_sock.override_flightplanner_waypoints(path_points, parsed_content['takeoff_alt'])
+            else:
+                print("Path points are None, no solution found...")
+        if command == Commands.PATH_GENERATION_POINT_TO_POINT:
+            self.path_gen.path_generation_type = path_generator.PathGenerationType.POINT_TO_POINT
+            path_points = self.path_gen.generate_path()
+            if path_points is not None:
+                mp_sock.override_flightplanner_waypoints(path_points, parsed_content['takeoff_alt'])
+            else:
+                print("Path points are None, no solution found...")
+        if command == Commands.PATH_GENERATION_FLY_TO_CIRCLE_TARGET:
+            self.path_gen.path_generation_type = path_generator.PathGenerationType.FLY_TO_CIRCLE_TARGET
+            path_points = self.path_gen.generate_path()
+            if path_points is not None:
+                mp_sock.override_flightplanner_waypoints(path_points, parsed_content['takeoff_alt'])
+            else:
+                print("Path points are None, no solution found...")
+        if command == Commands.PATH_GENERATION_FLY_TO_TARGET_PAYLOAD:
+            self.path_gen.path_generation_type = path_generator.PathGenerationType.FLY_TO_TARGET_PAYLOAD
+            path_points = self.path_gen.generate_path()
+            if path_points is not None:
+                mp_sock.override_flightplanner_waypoints(path_points, parsed_content['takeoff_alt'])
+            else:
+                print("Path points are None, no solution found...")
+
         if command == Commands.OVERRIDE_FLIGHTPLANNER:
             # Make instance of SearchPathGenerator
             waypoint_spliner = spliner.SearchPathGenerator()
@@ -80,14 +209,13 @@ class ServerHandler(BaseHTTPRequestHandler):
                                             paint_overlap=0.2)             # The percentage of overlap desired for the camera to see on consecutive layers
 
             # Generate and save spline
-            waypoint_spliner.generate_path()
+            waypoint_spliner.generate_search_area_path()
             splined_waypoints = waypoint_spliner.get_waypoints()  # A list of dictionaries with keys "long", "lat", and "alt" in order of flight
             mp_sock.override_flightplanner_waypoints(splined_waypoints, parsed_content['takeoff_alt'])
 
             # mp_socket.override_flightplanner_waypoints(parsed_content['waypoints'], parsed_content['takeoff_alt'])
             print("Executed OVERRIDE FLIGHTPLANNER WAYPOINTS")
         elif command == Commands.DIRECT_WAYPOINTS:
-            waypoint_spliner=directSpliner.PathGenerator()
             mp_sock.override_flightplanner_waypoints(parsed_content['waypoints'], parsed_content['takeoff_alt'])
         elif command == Commands.SYNC_SCRIPT:
             mp_sock.sync_script()
--- Backend/SplineGenerator/PathGenerator.py
+++ Backend/SplineGenerator/PathGenerator.py
@@ -1,10 +1,14 @@
 import PointToPointPathGenerator as ptpPG
 import SearchPathGenerator as saPG
+import FlyToCircleTarget as ftctPG
+import FlyToTargetPayload as fttpPG
 import math
 
 class PathGenerationType:
     SEARCH_AREA = "saPG"
     POINT_TO_POINT = "ptpPG"
+    FLY_TO_CIRCLE_TARGET = "ftctPG"
+    FLY_TO_TARGET_PAYLOAD = "fttpPG"
 
 class PathGenerator:
     def __init__(self):
@@ -18,6 +22,7 @@ class PathGenerator:
         # Common parameters
         self.minimum_turn_radius = None  # Metres
         self.curve_resolution = None  # Waypoints per metre on a curve
+        self.alt = None  # Altitude to print plots at
 
         # Search area specific data
         self.search_area = None
@@ -38,15 +43,29 @@ class PathGenerator:
         self.boundary_resolution = None
         self.boundary_tolerance = None
 
+        # Target specific parameters
+        self.plane_location = None
+        self.plane_bearing = None
+        self.target_location = None
+        self.target_circle_radius = None
+        self.minimum_distance_to_start = None
+        self.times_to_circle = None
+
     def generate_path(self):
         # Check what type of path generation is desired
         if self.path_generation_type == "saPG":  # Search area path generation
-            return self.handle_saPG()
+            return self.handle_search_area_PG()
 
         if self.path_generation_type == "ptpPG":  # Point to point path generation
-            return self.handle_ptpPG()
+            return self.handle_point_to_point_PG()
 
-    def handle_saPG(self):
+        if self.path_generation_type == "ftctPG":  # Fly to circle target path generation
+            return self.handle_fly_to_circle_target_PG()
+
+        if self.path_generation_type == "fttpPG":  # Fly to target payload path generation
+            return self.handle_fly_to_target_payload_PG()
+
+    def handle_search_area_PG(self):
         # Create class instance
         path_generator = saPG.SearchPathGenerator()
 
@@ -71,7 +90,8 @@ class PathGenerator:
                                       start_point=self.take_off_point,
                                       focal_length=self.focal_length,
                                       sensor_size=self.sensor_size,
-                                      paint_overlap=self.paint_overlap)
+                                      paint_overlap=self.paint_overlap,
+                                      alt=self.alt)
 
         # Generate path
         path_generator.generate_search_area_path(do_plot=self.do_plot)
@@ -79,7 +99,7 @@ class PathGenerator:
         # Return waypoints in dictionary format
         return path_generator.get_waypoints()
 
-    def handle_ptpPG(self):
+    def handle_point_to_point_PG(self):
         # Scale parameters
         scale_factor = 111320 / math.cos(self.waypoints[0].coords.lat)
         scaled_turn_radius = self.minimum_turn_radius / scale_factor
@@ -91,7 +111,8 @@ class PathGenerator:
                                                boundary_points=self.boundary_points,
                                                boundary_resolution=self.boundary_resolution,
                                                tolerance=self.boundary_tolerance,
-                                               curve_resolution=scaled_curve_resolution)
+                                               curve_resolution=scaled_curve_resolution,
+                                               alt=self.alt)
 
         # Do spline between waypoints
         path_generator.generate_spline()
@@ -100,3 +121,49 @@ class PathGenerator:
         if self.do_plot:
             path_generator.plot_waypoints()
 
+        # Get points in dict form
+        return path_generator.get_waypoints()
+
+    def handle_fly_to_circle_target_PG(self):
+        # Convert bearing from north being zero degrees to positive x-axis being zero degrees
+        bearing = self.plane_bearing
+        updated_bearing = - bearing + math.pi / 2
+
+        # Fill in parameters
+        fly_to_target = ftctPG.FlyToCircleTarget()
+        fly_to_target.set_parameters(plane_location=self.plane_location,
+                                     plane_bearing=updated_bearing,
+                                     target_location=self.target_location,
+                                     turn_radius=self.minimum_turn_radius,
+                                     target_circle_radius=self.target_circle_radius,
+                                     minimum_distance_to_start=self.minimum_distance_to_start,
+                                     times_to_circle=self.times_to_circle,
+                                     curve_resolution=self.curve_resolution)
+
+        path = fly_to_target.generate_path()
+        path_dict = fly_to_target.get_waypoints()
+
+        if self.do_plot:
+            ftctPG.plot_waypoints(path_dict)
+
+        return path_dict
+
+    def handle_fly_to_target_payload_PG(self):
+        path_generator = fttpPG.FlyToTargetPayload()
+
+        # Convert bearing from north being zero degrees to positive x-axis being zero degrees
+        bearing = 0
+        updated_bearing = - bearing + math.pi / 2
+
+        # Fill in parameters
+        path_generator.set_parameters(plane_location=self.plane_location,
+                                     plane_bearing=updated_bearing,
+                                     target_location=self.target_location,
+                                     turn_radius=self.minimum_turn_radius,
+                                     minimum_distance_to_start=self.minimum_distance_to_start,
+                                     curve_resolution=self.curve_resolution)
+
+        path = path_generator.generate_path()
+        if self.do_plot:
+            fttpPG.plot_waypoints(points_dict=path, point1=path_generator.target_location)
+        return path_generator.get_waypoints()
