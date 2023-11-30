c60d63ef910272befacdad586451ac729fd3ed3f
kluu0012 <kluu0012@student.monash.edu>
2023-11-30 15:04:07 +1100

    added weather vaining commands and status to backend

--- Backend/CommunicationScript/communication_script.py
+++ Backend/CommunicationScript/communication_script.py
@@ -450,6 +450,7 @@ class MissionManager:
                         Commands.TOGGLE_ARM: Commands.toggle_arm_aircraft,
                         Commands.GET_FLIGHTPLANNER_WAYPOINTS: Commands.get_flightplanner_waypoints,
                         Commands.SEND_COMMAND_INT: Commands.send_command_int,
+                        Commands.TOGGLE_WEATHER_VAINING: Commands.toggle_weather_vaining,
                         }  
         
         # run the command
@@ -516,6 +517,7 @@ class MissionManager:
                             "avionics_battery":float(self.cs_drone.battery_voltage2),
                             "armed": self.cs_drone.armed,
                             "drone_connected": self.drone_connected,
+                            "weather_vaining": Script.GetParam("WVAIN_ENABLE")
                             "messages": messages_to_send,
                             },
                         })
@@ -592,6 +594,19 @@ class MissionManager:
         except Exception as e:
             print("[ERROR] " + str(e))
             print("[ERROR] Failed to send COMMAND INT")
+    
+    
+    def toggle_weather_vaining(self):
+        """Toggles on and off weather vaining on the plane (Weather Vaining makes the drone face with the wind).
+        """
+        try:
+            status = Script.GetParam("WVAIN_ENABLE")
+            Script.SetParam("WVAIN_ENABLE", not status)
+            print("[INFO] Set Weather Vaining to: " + str(not status))
+        except Exception as e:
+            print("[ERROR] " + str(e))
+            print("[ERROR] Failed to toggle WEATHER VAINING, Current State: " + str(status))
+
 
     def handle_message_packet(self, raw_packet):
         """Handles any MAVLink message packets received, and prints if they are command_int or debug_vect messages
@@ -657,6 +672,7 @@ class Commands:
     LIVE_DRONE_DATA = "LIVE_DRONE_DATA"
     SET_CUBE_RELAY_PIN = "SET_CUBE_RELAY_PIN"
     SEND_COMMAND_INT = "SEND_COMMAND_INT"
+    TOGGLE_WEATHER_VAINING = "TOGGLE_WEATHER_VAINING"
 
 
     def override(self, mission_manager, decoded_data):
@@ -805,6 +821,16 @@ class Commands:
         except Exception as e:
             print("[ERROR] " + str(e))
             print("[COMMAND] ERROR: Handling MAV_COMMAND_INT COMMAND")
+
+
+    def toggle_weather_vaining(self, mission_manager, decoded_data):
+        """Toggles on and off weather vaining on the plane (Weather Vaining makes the drone face with the wind).
+        """
+        try:
+            mission_manager.toggle_weather_vaining()
+        except Exception as e:
+            print("[ERROR] " + str(e))
+            print("[ERROR] ERROR: Handling TOGGLE_WEATHER_VAINING COMMAND")
 
 # ------------------------------------ End Classes ------------------------------------
 # def waypoint_mavlink_test():
