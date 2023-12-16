70452cdf2851205916a1b83e936303fdc34be449
Incy <ilin0005@student.monash.edu>
2023-12-16 13:52:58 +1100

    fixed modal popping up when no vision data

--- BatchFile/missionManagement.bat
+++ BatchFile/missionManagement.bat
@@ -1,8 +1,8 @@
 @echo off
-start cmd.exe /c "cd <Paste complete path to Mission Management folder> && npm run serve"
-rem Example for above: start cmd.exe /c "cd C:\Users\winky\Documents\MUAS\Mission-Management && npm run serve"
-start cmd.exe /c "python <Paste complete path to the DronelinkServer.py file>"
-rem Example for above: start cmd.exe /c "python C:\Users\winky\Documents\MUAS\Mission-Management\Backend\DronelinkServer.py"
+rem start cmd.exe /c "cd <Paste complete path to Mission Management folder> && npm run serve"
+start cmd.exe /c "cd C:\Users\winky\Documents\MUAS\Mission-Management && npm run serve"
+rem start cmd.exe /c "python <Paste complete path to the DronelinkServer.py file>"
+start cmd.exe /c "python C:\Users\winky\Documents\MUAS\Mission-Management\Backend\DronelinkServer.py"
 
 set ip_address_string="IPv4 Address"
 rem Edit "Chrome" below to your browser if you do not have Chrome
