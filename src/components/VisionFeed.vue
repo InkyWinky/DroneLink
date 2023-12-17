6436b675dbbd2c7299dd7b6e5eb23e03420267d7
kluu0012 <kluu0012@student.monash.edu>
2023-12-17 20:24:26 +1100

    fixed vision resolution and added no vision state

--- src/components/VisionFeed.vue
+++ src/components/VisionFeed.vue
@@ -3,20 +3,19 @@
     class="uk-card uk-card-default uk-card-body p-2"
     style="border-radius: 15px"
   >
-    <router-link
-      to="/payload"
-      class="w-full h-full items-center justify-center"
-    >
-      <h3>VISION FEED</h3>
-    </router-link>
-
+    <h3>VISION FEED</h3>
     <div class="bg-gray-500 rounded-md border border-black" style="height: 90%">
-      <div
-        v-if="!has_feed"
-        uk-icon="icon:camera; ratio: 2"
-        class="flex flex-col justify-center items-center text-white relative"
-      ></div>
-      <img v-show="has_feed" ref="vision_feed" class="h-full w-full" />
+      <router-link
+        to="/payload"
+        class="w-full h-full flex items-center justify-center relative"
+      >
+        <div
+          v-if="!has_feed"
+          uk-icon="icon:camera; ratio: 2"
+          class="flex flex-col justify-center items-center text-white relative"
+        ></div>
+        <img v-show="has_feed" ref="vision_feed" class="h-[100%] w-fit" />
+      </router-link>
     </div>
   </div>
 </template>
