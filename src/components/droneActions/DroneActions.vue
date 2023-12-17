ec2398a4f652c6224180407513243c1671469759
kluu0012 <kluu0012@student.monash.edu>
2023-12-18 10:54:05 +1100

    added loiter button in drone actions

--- src/components/droneActions/DroneActions.vue
+++ src/components/droneActions/DroneActions.vue
@@ -4,22 +4,23 @@
     style="border-radius: 15px; padding: 30px; bottom: 4%"
   >
     <h3>DRONE ACTIONS</h3>
-    <div
-      id="rtl-container"
-      class="h-auto w-full m-2 p-2 bg-slate-200 action-btn"
-    >
+    <div class="h-auto w-full m-2 p-2 bg-slate-200 action-btn">
       <button
-        id="rtl-btn"
+        class="w-full h-auto rounded p-2 flex items-center justify-center"
+        @click="setLoiter()"
+      >
+        LOITER
+      </button>
+    </div>
+    <div class="h-auto w-full m-2 p-2 bg-slate-200 action-btn">
+      <button
         class="w-full h-auto rounded p-2 flex items-center justify-center"
         @click="RTL()"
       >
         RTL
       </button>
     </div>
-    <div
-      id="qloiter-container"
-      class="action-btn h-auto w-full m-2 p-2 bg-slate-200"
-    >
+    <div class="action-btn h-auto w-full m-2 p-2 bg-slate-200">
       <button
         id="qloiter-btn"
         class="w-full h-auto rounded p-2 flex items-center justify-center"
@@ -33,6 +34,9 @@
 <script setup>
 import api from "@/api";
 
+function setLoiter() {
+  api.executeCommand("CHANGE_DRONE_MODE", { mode: "LOITER" });
+}
 function RTL() {
   api.executeCommand("CHANGE_DRONE_MODE", { mode: "RTL" });
 }
