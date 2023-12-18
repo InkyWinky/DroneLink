2a55540f882fe8aa1ce2c19f32df5f806d1c7429
Incy <ilin0005@student.monash.edu>
2023-12-18 13:15:11 +1100

    some fixes

--- src/components/pathPlanner/PathPlanner.vue
+++ src/components/pathPlanner/PathPlanner.vue
@@ -393,7 +393,7 @@ async function formatWaypoints() {
     takeoff_alt: takeoff_alt,
     cruise_alt: store?.settings?.default_alt,
     vtol_transition_mode: store?.settings?.vtol_transition_mode,
-    drone_location: { lat: store?.live_data?.lat, long: store?.live_data?.lng },
+    drone_location: { lat: store?.live_data?.lat, lng: store?.live_data?.lng },
     min_turn_radius: store?.settings?.min_turn_radius,
   });
 
